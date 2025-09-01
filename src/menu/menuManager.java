b296ddbbfaea1d02d93670ffec7db9e8ebb6f047
Ethan Seulin <ethan.seulin.etu@univ-lille.fr>
2025-09-01 17:21:04 +0200

    aa

--- src/menu/menuManager.java
+++ src/menu/menuManager.java
@@ -1,22 +1,37 @@
 package src.menu;
 
 import java.util.Scanner;
+import src.main.java.Main;
+import src.menu.menuRules;
 
 public class menuManager {
-    public String menuPrincipal() {
+    public void menuPrincipal() {
         Scanner scanner = new Scanner(System.in);
         afficherMenuPrincipal();
         String choice = scanner.nextLine();
         scanner.close();
-        return choice;
+
+        if(choice=="1") {
+            return Main();
+        }
+
+        if (choice="2") {
+            menuRegles();
+        }
+
+        if(choice="4") {
+            System.exit(0);
+        }
+
+
     }
 
     public void afficherMenuPrincipal() {
         System.out.println("\n\n\n\n\n\n\n");
-        System.out.println("               Jouer");
-        System.out.println("               Règles");
-        System.out.println("               Leaderboard");
-        System.out.println("               Quitter\n\n\n\n\n");
+        System.out.println("             [1] Jouer");
+        System.out.println("             [2] Règles");
+        System.out.println("             [3] Leaderboard");
+        System.out.println("             [4] Quitter\n\n\n\n\n");
         System.out.println("[>] Choisissez votre option: ");
     }
 }
--- src/menu/menuRules.java
+++ src/menu/menuRules.java
@@ -54,8 +54,4 @@ public class menuRules {
         scanner.nextLine();
         scanner.close();
     }
-
-    public static void main(String[] args) {
-        menuRegles();
-    }
 }
