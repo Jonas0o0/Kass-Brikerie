ebc4158451d4f63176db9b869e654aee5ca28b67
Alban Sonneville <alban.sonneville.etu@univ-lille.fr>
2025-09-01 16:42:43 +0200

    Update file brique.java

--- brique.java
+++ brique.java
@@ -6,18 +6,23 @@ public class brique{
 
     public int posX; // Position sur l'axe X
     public int posY; // Position sur l'axe Y
-    public String design = "###"; // Design de la brique
+    public String affichage = "###"; // Affichage de la brique
     public int score = 10; // Rajoute 10 au score en cas de destruction de la brique 
+
+    // Affiche la brique
+    void afficherBrique(){System.out.print(this.affichage);}
     
     // Positionnement de la brique aux coordonnées placées en paramètres
     void positionBrique(int positionX, int positionY){
 
         this.posX = positionX;
         this.poxY = positionY;
-    
-        SetCursorPosition(this.posY, this.posX);
-        System.out.print(this.design);
+
+        // Déplacer le curseur à la ligne Y, colonne X
+
+        System.out.print(this.afficherBrique());
         return;
     }
+
 
 }
