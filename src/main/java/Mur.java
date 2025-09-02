06fdc9f2676f4194489aa6d8a4c89175fa40dc27
Mano LEMAIRE <mano.lemaire.etu@univ-lille.fr>
2025-09-02 09:12:06 +0200

    Générer mur

--- src/main/java/Mur.java
+++ src/main/java/Mur.java
@@ -13,9 +13,15 @@ public class Mur{
         mur = new Brique[taille];
     }
 
-    void genereLigne(int x , int y, int nbBriques){
+    void genererLigne(int x , int y, int nbBriques){
         for(int i = 0; i<nbBriques; i++){
             mur[i] = new Brique(x + i*(Brique.longeur+1) , y);
+        }
+    }
+
+    void genererMur(int x, int y, int hauteur, int longueur){
+        for(int i = 0; i<hauteur; i++){
+            genererLigne(x, y + i*(Brique.hauteur+1), longueur);
         }
     }
 
