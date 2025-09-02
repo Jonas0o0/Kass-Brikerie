6a4d5f7f91bf894b1656709a3899f0f8a9632e36
Alban Sonneville <alban.sonneville.etu@univ-lille.fr>
2025-09-02 08:12:25 +0200

    Ajout des getPosition pour la brique

--- src/main/java/Brique.java
+++ src/main/java/Brique.java
@@ -9,6 +9,19 @@ public class Brique{
     public int posY; // Position sur l'axe Y
     public int score = 10; // Rajoute 10 au score en cas de destruction de la brique 
 
+    Brique(int positionX, int positionY){
+        this.posX = positionX;
+        this.posY = positionY;
+    }
+
+    public int getPositionX() {
+        return this.posX;
+    }
+
+    public int getPositionY() {
+        return this.posY;
+    }
+
     // Affiche la brique
     void afficherBrique(){
         System.out.print(" ...... \n|......|");
