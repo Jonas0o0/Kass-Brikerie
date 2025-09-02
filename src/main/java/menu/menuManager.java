cb3113a4b0f44f30c1594291fe9fe9b43dbeb49b
Ethan Seulin <ethan.seulin.etu@univ-lille.fr>
2025-09-02 08:20:50 +0200

    menu integ autres classes + verif choix

--- src/main/java/menu/menuManager.java
+++ src/main/java/menu/menuManager.java
@@ -1,37 +1,52 @@
 package src.main.java.menu;
 
 import java.util.Scanner;
+
 import src.main.java.Main;
-import src.main.java.menu.menuRules;
 
 public class menuManager {
-    public void menuPrincipal() {
+    public static void menuPrincipal() {
+        String choice;
         Scanner scanner = new Scanner(System.in);
-        afficherMenuPrincipal();
-        String choice = scanner.nextLine();
-        scanner.close();
+        do {
+            afficherMenuPrincipal();
+            choice = scanner.nextLine();
+        } while(!isNumer(choice));
 
-        if(choice=="1") {
-            Main();
+
+        if(choice.equals("1")) {
+            Main.main(null);
         }
 
-        if (choice="2") {
-            menuRegles();
+        if (choice.equals("2")) {
+            menuRules.menuRegles();
         }
 
-        if(choice="4") {
+        if(choice.equals("4")) {
             System.exit(0);
         }
-
-
+        scanner.close();
     }
 
-    public void afficherMenuPrincipal() {
+    public static void afficherMenuPrincipal() {
         System.out.println("\n\n\n\n\n\n\n");
         System.out.println("             [1] Jouer");
         System.out.println("             [2] Règles");
         System.out.println("             [3] Leaderboard");
         System.out.println("             [4] Quitter\n\n\n\n\n");
         System.out.println("[>] Choisissez votre option: ");
+    }
+
+    private static boolean isNumer(String stringNombre) {
+        try {
+            Integer.parseInt(stringNombre);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        menuPrincipal();
     }
 }
